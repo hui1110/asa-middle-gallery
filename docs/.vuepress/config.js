35e5f82c7b61d1b9e80ba8266df6d7e2a5997684
hui1110 <hui1110rui@163.com>
2023-09-01 18:43:01 +0800

    update github action file

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,7 @@
 module.exports = {
+    head: [
+        ['link', { rel: 'icon', href: '/assets/Microsoft_Azure.svg' }],
+    ],
     title: "Azure Spring Apps button Documentation",
     description: "From code to deployment in minutes",
     base: "/asa-middle-gallery/",
@@ -43,13 +46,5 @@ module.exports = {
           notFoundPath: '/404.html',
         },
       ],
-    ],
-    head: [
-      ['script', { async: '', defer: '' , src: 'https://buttons.github.io/buttons.js' }],
-      ['script', {}, `(function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "4zmkonp2tw");` ]
     ]
   };
